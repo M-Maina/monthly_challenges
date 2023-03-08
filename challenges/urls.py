d29038345f4792d43fb15e31c534bcd4f3bc07da
wamah <pythondeveloper21@gmail.com>
2023-03-08 21:09:15 +0000

    Redirect using Reverse

--- challenges/urls.py
+++ challenges/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     #path('january/', views.index ),
     #path('february/', views.feb ),
     path("<int:month>", views.monthly_challenge_by_number),
-    path("<str:month>", views.monthly_challenge),
+    path("<str:month>", views.monthly_challenge, name="month-challenge"),
     
 ]
